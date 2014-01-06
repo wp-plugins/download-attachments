80955b5e6dc059bc076868ae1bfe74db0053eb96
dfactory <dfactory@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-06 15:05:45 +0000

    Tagging version 1.0.5
    git-svn-id: https://plugins.svn.wordpress.org/download-attachments/tags/1.0.5@833908 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/shortcodes.php
@@ -0,0 +1,95 @@
+<?php
+if(!defined('ABSPATH'))	exit; //exit if accessed directly
+
+new Download_Attachments_Shortcodes();
+
+class Download_Attachments_Shortcodes
+{
+	private $options = array();
+
+
+	/**
+	 * Class constructor
+	*/
+	public function __construct()
+	{
+		//settings
+		$this->options = array_merge(
+			array('general' => get_option('download_attachments_general'))
+		);
+
+		//actions
+		add_action('init', array(&$this, 'register_download_shortcodes'));
+	}
+
+
+	/**
+	 * Registers download attachments shortcodes
+	*/
+	public function register_download_shortcodes()
+	{
+		add_shortcode('download-attachments', array(&$this, 'download_attachments_shortcode'));
+		add_shortcode('download-attachment', array(&$this, 'download_attachment_shortcode'));
+	}
+
+
+	/**
+	 * Handles download-attachments shortcode
+	*/
+	public function download_attachments_shortcode($args)
+	{
+		$defaults = array(
+			'container' => 'div',
+			'container_class' => 'download-attachments',
+			'container_id' => '',
+			'style' => 'list',
+			'link_before' => '',
+			'link_after' => '',
+			'display_user' => (int)$this->options['general']['frontend_columns']['author'],
+			'display_icon' => (int)$this->options['general']['frontend_columns']['icon'],
+			'display_count' => (int)$this->options['general']['frontend_columns']['downloads'],
+			'display_size' => (int)$this->options['general']['frontend_columns']['size'],
+			'display_date' => (int)$this->options['general']['frontend_columns']['date'],
+			'display_caption' => (int)$this->options['general']['frontend_content']['caption'],
+			'display_description' => (int)$this->options['general']['frontend_content']['description'],
+			'display_empty' => 0,
+			'display_option_none' => __('No attachments to download', 'download-attachments'),
+			'use_desc_for_title' => 0,
+			'exclude' => '',
+			'include' => '',
+			'title' => __('Download Attachments', 'download-attachments'),
+			'orderby' => 'menu_order',
+			'order' => 'asc',
+			'echo' => 1
+		);
+
+		//we have to force return in shortcodes
+		$args['echo'] = 0;
+
+		if(!isset($args['title']))
+		{
+			$args['title'] = '';
+
+			if($this->options['general']['label'] !== '')
+			$args['title'] = $this->options['general']['label'];
+		}
+
+		return da_display_download_attachments((int)get_queried_object_id(), shortcode_atts($defaults, $args));
+	}
+
+
+	/**
+	 * Handles download-attachment shortcode
+	*/
+	public function download_attachment_shortcode($args)
+	{
+		$defaults = array(
+			'attachment_id' => 0
+		);
+
+		$args = shortcode_atts($defaults, $args);
+
+		return da_download_attachment_link((int)$args['attachment_id'], FALSE);
+	}
+}
+?>
